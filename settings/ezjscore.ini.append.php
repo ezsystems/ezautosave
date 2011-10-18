c81e2eaedb2a4688f6c1aead8427d55312a7d611
NGUYEN DINH Quoc-Huy <contact@quoc-huy.com>
2011-10-19 00:23:44 +0200

    remove example comment

--- settings/ezjscore.ini.append.php
+++ settings/ezjscore.ini.append.php
@@ -4,8 +4,6 @@
 FunctionList[]=qhautosave
 
 [ezjscServer_qhautosave]
-# Url to test this server function:
-# <root>/ezjscore/call/ezstarrating::rate::<contentobjectattribute_id>::<version>::<rating>
 Class=qhASServerFunctions
 Functions[]=qhautosave
 File=extension/qhautosave/classes/qhasserverfunctions.php
