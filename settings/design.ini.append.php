d9cce319f43eb03a54fd6543eda79906c423dfa1
NGUYEN DINH Quoc-Huy <contact@quoc-huy.com>
2011-11-26 16:40:20 +0100

    Add a workaround when jQuery is loaded after the qhautosave.js file

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -4,6 +4,12 @@
 DesignExtensions[]=qhautosave
 
 [JavaScriptSettings]
+# If QH AutoSave doesn't work with the frontend editor, it might be
+# because jQuery is loaded after QH AutoSave.
+# Try to uncomment the two following lines and clear the caches
+# FrontendJavaScriptList[]=ezjsc::jquery
+# FrontendJavaScriptList[]=ezjsc::jqueryio
+
 FrontendJavaScriptList[]=qhautosave.js
 BackendJavaScriptList[]=qhautosave.js
 
