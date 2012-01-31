a3a302a5ee224a7c4db785c8ed6e43b08d1c66e0
Damien Pobel <dp@ez.no>
2012-01-31 10:24:28 +0100

    Added up to date translation files

--- translations/untranslated/translation.ts
+++ translations/untranslated/translation.ts
@@ -5,72 +5,72 @@
     <name>design/admin2/autosave</name>
     <message>
         <source>An error occured while autosaving the draft</source>
-        <translation>Une erreur s'est produite durant l'enregistrement automatique du brouillon</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>The draft is being saved</source>
-        <translation>Le brouillon est en cours de d'enregistrement</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>design/ezwebin/autosave</name>
     <message>
         <source>An error occured while autosaving the draft</source>
-        <translation>Une erreur s'est produite durant l'enregistrement automatique du brouillon</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>The draft is being saved</source>
-        <translation>Le brouillon est en cours de d'enregistrement</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>extension/ezautosave</name>
     <message>
         <source>Draft saved at %time</source>
-        <translation>Brouillon enregistré à %time</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>(%min minutes ago)</source>
-        <translation>(Il y a %min minutes)</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>extension/ezautosave/autosave</name>
     <message>
         <source>This action requires 3 parameters</source>
-        <translation>Cette action attend 3 paramètres</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>A POST request is expected</source>
-        <translation>Une requête de type POST est attendue</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>No POST data found, it&apos;s probably because you tried to upload a too big file</source>
-        <translation>Aucune donnée n'a été transmise en POST, vous essayez probablement d'envoyer un fichier trop gros</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Unable to load the content #%objectid</source>
-        <translation>Impossible de trouver le contenu #%objectid</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>The content #%objectid is archived</source>
-        <translation>Le contenu #%objectid is archived</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Unable to load version #%versionr of content #%objectid</source>
-        <translation>Impossible de trouver la version #%versionr du contenu #%objectid</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Version #%versionr of content #%objectid is not a draft</source>
-        <translation>La version #%versionr du contenu #%objectid n'est pas un brouillon</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>You&apos;re not allowed to store a version that is not yours</source>
-        <translation>Vous n'avez pas le droit de modifier une version dont vous n'êtes pas le créateur</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <source>Unable to load the language &apos;%localeCode&apos;</source>
-        <translation>Impossible de trouver la langue '%localeCode'</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
