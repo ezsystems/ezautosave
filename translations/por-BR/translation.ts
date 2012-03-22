342b49bd57636ac32ed9374242f2033439d130d5
Tutei Admin <thiagocamposviana@gmail.com>
2012-03-23 00:30:37 +0100

    Finalized pt-BR translation

---
+++ translations/por-BR/translation.ts
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>design/admin2/autosave</name>
+    <message>
+        <source>An error occured while autosaving the draft</source>
+        <translation>Ocorreu um erro durante o salvamento automático do rascunho</translation>
+    </message>
+    <message>
+        <source>The draft is being saved</source>
+        <translation>O rascunho está sendo salvo</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin2/content/ajax_preview</name>
+    <message>
+        <source>Back to the edit form</source>
+        <translation>Retornar para o formulário de edição</translation>
+    </message>
+    <message>
+        <source>Preview of &amp;lt;%name&amp;gt; in siteaccess</source>
+        <translation>Pré-visualização de &amp;lt;%name&amp;gt; no siteaccess</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin2/preview</name>
+    <message>
+        <source>Loading...</source>
+        <translation>Carregando...</translation>
+    </message>
+    <message>
+        <source>An error occured.</source>
+        <translation>Ocorreu um erro.</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Pré-visualição</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/autosave</name>
+    <message>
+        <source>An error occured while autosaving the draft</source>
+        <translation>Ocorreu um erro durante o salvamento automático do rascunho</translation>
+    </message>
+    <message>
+        <source>The draft is being saved</source>
+        <translation>O rascunho está sendo salvo</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezautosave</name>
+    <message>
+        <source>Draft saved at %time</source>
+        <translation>Rascunho salvo em %time</translation>
+    </message>
+    <message>
+        <source>(%min minutes ago)</source>
+        <translation>(%min minutos atrás)</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezautosave/autosave</name>
+    <message>
+        <source>This action requires 3 parameters</source>
+        <translation>Esta ação requer 3 parâmetros</translation>
+    </message>
+    <message>
+        <source>A POST request is expected</source>
+        <translation>Uma requisição POST é esperada</translation>
+    </message>
+    <message>
+        <source>No POST data found, it&apos;s probably because you tried to upload a too big file</source>
+        <translation>Nenhum dado do tipo POST foi encontrado, provavelmente porque você tentou enviar um arquivo muito grande</translation>
+    </message>
+    <message>
+        <source>Unable to load the content #%objectid</source>
+        <translation>Impossível carregar o conteúdo #%objectid</translation>
+    </message>
+    <message>
+        <source>The content #%objectid is archived</source>
+        <translation>O conteúdo #%objectid foi arquivado</translation>
+    </message>
+    <message>
+        <source>Unable to load version #%versionr of content #%objectid</source>
+        <translation>Não foi possível carregar a versão #%versionr do conteúdo #%objectid</translation>
+    </message>
+    <message>
+        <source>Version #%versionr of content #%objectid is not a draft</source>
+        <translation>A versão #%versionr do conteúdo #%objectid não é um rascunho</translation>
+    </message>
+    <message>
+        <source>You&apos;re not allowed to store a version that is not yours</source>
+        <translation>É proíbido armazenar uma versão que não te pertence</translation>
+    </message>
+    <message>
+        <source>Unable to load the language &apos;%localeCode&apos;</source>
+        <translation>Impossível carregar o idioma &apos;%localeCode&apos;</translation>
+    </message>
+</context>
+</TS>
