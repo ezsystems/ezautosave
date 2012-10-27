ece0130820c6bd64849390a5896bb218256a3f4c
karol <karol_trolek@o2.pl>
2012-10-27 21:46:38 +0200

    localization: added pol-PL translation.ts (cherry picked from commit 470e6e2caea477de8588a2d33c16f5d4852b7498)

---
+++ translations/pol-PL/translation.ts
@@ -0,0 +1,106 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>design/admin2/autosave</name>
+    <message>
+        <source>An error occurred while autosaving the draft</source>
+        <translation>Wystąpił błąd podczas automatycznego zapisywania szkicu</translation>
+    </message>
+    <message>
+        <source>The draft is being saved</source>
+        <translation>Szkic jest zapisywany</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin2/content/ajax_preview</name>
+    <message>
+        <source>Back to the edit form</source>
+        <translation>Powrót do formularza edycji</translation>
+    </message>
+    <message>
+        <source>Preview of &amp;lt;%name&amp;gt; in siteaccess</source>
+        <translation>Podgląd &amp;lt;%name&amp;gt; w dostępie do strony</translation>
+    </message>
+    <message>
+        <source>&lt;strong&gt;Warning:&lt;/strong&gt; &lt;em&gt;following links from the preview will take you to the live version of the website&lt;/em&gt;</source>
+        <translation>&lt;strong&gt;Uwaga:&lt;/strong&gt; &lt;em&gt;Poniższe odnośniki z podglądu przeniosą cię do właściwej wersji strony&lt;/em&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>design/admin2/preview</name>
+    <message>
+        <source>Loading...</source>
+        <translation>Wczytywanie...</translation>
+    </message>
+    <message>
+        <source>An error occurred.</source>
+        <translation>Wystąpił błąd.</translation>
+    </message>
+    <message>
+        <source>Preview</source>
+        <translation>Podgląd</translation>
+    </message>
+</context>
+<context>
+    <name>design/ezwebin/autosave</name>
+    <message>
+        <source>An error occurred while autosaving the draft</source>
+        <translation>Wystąpił błąd podczas automatycznego zapisywania szkicu</translation>
+    </message>
+    <message>
+        <source>The draft is being saved</source>
+        <translation>Szkic jest zapisywany</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezautosave</name>
+    <message>
+        <source>Draft saved at %time</source>
+        <translation>Szkic zapisany o %time</translation>
+    </message>
+    <message>
+        <source>(%min minutes ago)</source>
+        <translation>(%min minut temu)</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ezautosave/autosave</name>
+    <message>
+        <source>This action requires 3 parameters</source>
+        <translation>Ta akcja wymaga trzech parametrów</translation>
+    </message>
+    <message>
+        <source>A POST request is expected</source>
+        <translation>Oczekuje się żądania POST</translation>
+    </message>
+    <message>
+        <source>No POST data found, it&apos;s probably because you tried to upload a too big file</source>
+        <translation>Nie znaleziono żadnych danych POST, prawdopodobnie próbowano przesłać zbyt duży plik</translation>
+    </message>
+    <message>
+        <source>Unable to load the content #%objectid</source>
+        <translation>Nie można wczytać obiektu #%objectid</translation>
+    </message>
+    <message>
+        <source>The content #%objectid is archived</source>
+        <translation>Obiekt #%objectid jest archiwizowany</translation>
+    </message>
+    <message>
+        <source>Unable to load version #%versionr of content #%objectid</source>
+        <translation>Nie można załadować wesji #%versionr obiektu #%objectid</translation>
+    </message>
+    <message>
+        <source>Version #%versionr of content #%objectid is not a draft</source>
+        <translation>Wersja #%versionr obiektu #%objectid nie jest szkicem</translation>
+    </message>
+    <message>
+        <source>You&apos;re not allowed to store a version that is not yours</source>
+        <translation>Nie masz uprawnień do zapisu wersji, której nie jesteś właścicielem</translation>
+    </message>
+    <message>
+        <source>Unable to load the language &apos;%localeCode&apos;</source>
+        <translation>Nie można wczytać języka &apos;%localeCode&apos;</translation>
+    </message>
+</context>
+</TS>
